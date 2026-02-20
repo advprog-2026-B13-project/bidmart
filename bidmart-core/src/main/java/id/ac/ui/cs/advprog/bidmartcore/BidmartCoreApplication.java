6ae887657ec8d156007f6f8ba7eb68ca6c4f0ad8
qowiys <qowiyshabir1416@gmail.com>
2026-02-20 14:27:14 +0700

    chore(core): initialize bidmart-core

---
+++ bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/BidmartCoreApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartCoreApplication.class, args);
+    }
+
+}
---
+++ bidmart-core/src/test/java/id/ac/ui/cs/advprog/bidmartcore/BidmartCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidmartCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
