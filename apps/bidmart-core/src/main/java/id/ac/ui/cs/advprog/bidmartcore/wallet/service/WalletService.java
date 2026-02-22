88c39f044ec12a17e7758429cbac7ad9c286e25a
Mednoob <ahmadyaqdhan123@gmail.com>
2026-02-22 07:35:06 +0700

    chore(core): define structure

---
+++ apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/wallet/service/WalletService.java
@@ -0,0 +1,4 @@
+package id.ac.ui.cs.advprog.bidmartcore.wallet.service;
+
+// Ini hanya template / placeholder aja buat dasar struktur projek, tar hapus kalau gak kepake atau mau pake nama lain ;)
+public class WalletService {}
