5ae12af50c41d4a5da6aee474a21f61989041ed9
Mednoob <ahmadyaqdhan123@gmail.com>
2026-05-01 20:49:06 +0700

    feat(core): add more permissions

--- apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/auth/domain/model/enums/PermissionValue.java
+++ apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/auth/domain/model/enums/PermissionValue.java
@@ -8,8 +8,22 @@ import lombok.Getter;
 public enum PermissionValue {
     AUCTION_CREATE("auction:create"),
     AUCTION_DELETE("auction:delete"),
+
+    CATALOG_CREATE_CATEGORY("catalog:create_category"),
+    CATALOG_UPDATE_CATEGORY("catalog:update_category"),
+    CATALOG_DELETE_CATEGORY("catalog:delete_category"),
+
+    LISTING_CREATE_LISTING("listing:create_listing"),
+    LISTING_UPDATE_LISTING("listing:update_listing"),
+    LISTING_DELETE_LISTING("listing:delete_listing"),
+
+    ORDER_UPDATE_SHIPMENT_STATUS("order:update_shipment_status"),
+    ORDER_CONFIRM_DELIVERY("order:confirm_delivery"),
+
     ACCOUNT_DEACTIVATE("account:deactivate"),
+
     USER_MANAGE("user:manage"),
+
     ADMIN("admin:all")
     ;
 
