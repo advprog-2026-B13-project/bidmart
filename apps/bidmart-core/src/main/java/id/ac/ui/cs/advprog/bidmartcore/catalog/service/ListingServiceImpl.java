f14a9a3ec4e65841ddef9a5f89a9882efb661bf9
ruzzy13 <uzma.zhafira@gmail.com>
2026-05-21 16:27:39 +0700

    fix(catalog): add missing @Slf4j annotation in ListingServiceImpl

--- apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/catalog/service/ListingServiceImpl.java
+++ apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/catalog/service/ListingServiceImpl.java
@@ -23,6 +23,7 @@ import id.ac.ui.cs.advprog.bidmartcore.catalog.repository.ListingSpecification;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service("catalogListingService")
 public class ListingServiceImpl implements ListingService {
 
