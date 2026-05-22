34a0aaafea015525b349e217fa5428f76623118a
Mednoob <ahmadyaqdhan123@gmail.com>
2026-05-22 18:07:33 +0700

    fix(core): fix missing import

--- apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/order/service/OrderServiceImpl.java
+++ apps/bidmart-core/src/main/java/id/ac/ui/cs/advprog/bidmartcore/order/service/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.bidmartcore.order.service;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
